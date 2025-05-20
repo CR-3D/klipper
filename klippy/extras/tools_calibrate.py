205050d43b166b552d61b318cafb618d0af09539
benecito <benedikt.daschner@gmail.com>
2025-05-20 13:33:58 +0200

    updated source reference of tools_calibrate

--- klippy/extras/tools_calibrate.py
+++ klippy/extras/tools_calibrate.py
@@ -1,7 +1,7 @@
 # Nozzle alignment module for 3d kinematic probes.
 #
-# This module has been adapted from code written by Kevin O'Connor <kevin@koconnor.net> and Martin Hierholzer <martin@hierholzer.info>
-# Sourced from https://github.com/ben5459/Klipper_ToolChanger/blob/master/probe_multi_axis.py
+# This module has been adapted from code written by Kevin O'Connor <kevin@koconnor.net> and Martin Hierholzer <martin@hierholzer.info> as well as Viesturs Zariņš <viesturz@gmail.com>
+# Sourced from https://github.com/ben5459/Klipper_ToolChanger/blob/master/probe_multi_axis.py and https://github.com/viesturz/klipper-toolchanger/blob/main/klipper/extras/tools_calibrate.py
 
 import logging
 
